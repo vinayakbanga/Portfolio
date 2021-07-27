54e555b9dca937042205a6d82bf51f98b24e9ad5
vinayakbanga <vinayakbanga22@gmail.com>
2021-07-28 00:33:57 +0530

    transition left and optimation left

---
+++ Particle/script.js
@@ -0,0 +1,9 @@
+// $('.about-btn').click(function(){
+//     $('html,body').animate({scrollTop:$('.about').offset().top},1000)
+// });
+$document.ready(function(){
+    alert(("hello"));
+    // $('h1').click(function(){
+    //     $(this).css('border','solid')
+    // })
+});
